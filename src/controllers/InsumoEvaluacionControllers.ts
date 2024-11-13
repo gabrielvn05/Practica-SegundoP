3c684c7d6c563dc5026bdfa30bee9e7bcd884299
gabrielvn05 <gabrielalexanderveleznunez@gmail.com>
2024-11-12 22:42:05 -0500

    Segundo commint del segundo parcial, correccion

--- src/controllers/InsumoEvaluacionControllers.ts
+++ src/controllers/InsumoEvaluacionControllers.ts
@@ -12,15 +12,9 @@ export class InsumoEvaluacionController {
 
   async create(req: Request, res: Response) {
     const insumoDto = req.body as CreateInsumoDto;
-
-    // Asegurarse de que los valores no sean indefinidos o nulos
-    if (insumoDto.ID_Pregunta == null || insumoDto.ID_Examen == null) {
-      return res.status(400).json({ message: 'ID_Pregunta y ID_Examen son requeridos' });
-    }
-
     const insumo = new InsumoEvaluacion();
-    insumo.ID_Pregunta = insumoDto.ID_Pregunta;
-    insumo.ID_Examen = insumoDto.ID_Examen;
+    //insumo.ID_Pregunta = insumoDto.ID_Pregunta;
+    //insumo.ID_Examen = insumoDto.ID_Examen;
     insumo.Valor = insumoDto.Valor;
 
     const nuevoInsumo = await this.repository.create(insumo);
@@ -45,18 +39,11 @@ export class InsumoEvaluacionController {
       return res.status(404).json({ message: 'Insumo de evaluación no encontrado' });
     }
 
-    // Asegurarse de que los valores no sean indefinidos o nulos
-    if (insumoDto.ID_Pregunta != null) {
-      insumo.ID_Pregunta = insumoDto.ID_Pregunta;
-    }
-    if (insumoDto.ID_Examen != null) {
-      insumo.ID_Examen = insumoDto.ID_Examen;
-    }
-    if (insumoDto.Valor != null) {
-      insumo.Valor = insumoDto.Valor;
-    }
+    //insumo.ID_Pregunta = insumoDto.ID_Pregunta;
+    //insumo.ID_Examen = insumoDto.ID_Examen;
+    insumo.Valor = insumoDto.Valor;
 
-    const insumoActualizado = await this.repository.update(insumo);
-    res.status(200).json(insumoActualizado);
+    //const insumoActualizado = await this.repository.update(insumo);
+    //res.status(200).json(insumoActualizado);
   }
 }
